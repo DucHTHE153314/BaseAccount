008c0fcd82315967a805bc2c4a76a93f026b96e9
DucHE153314 <ductranhoangk35dlnq@gmail.com>
2022-05-13 16:03:07 +0700

    forgot pass

--- app/models/CustomerLogics.php
+++ app/models/CustomerLogics.php
@@ -92,7 +92,7 @@ class CustomerLogics extends CustomerDB
         $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
         $randstring = '';
         for ($i = 0; $i < 10; $i++) {
-            $randstring = $characters[rand(0, strlen($characters))];
+            $randstring .= $characters[rand(0, strlen($characters))];
         }
         return $randstring;
     }
